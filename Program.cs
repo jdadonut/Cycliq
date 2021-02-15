01e30a6f14d7c8ae38f063994f5722619c4d0935
Jai L <47060357+jdadonut@users.noreply.github.com>
2021-02-15 11:51:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using Microsoft.Extensions.Configuration;
+
+namespace Cycliq
+{
+    internal class Program
+    {
+        private CancellationTokenSource __ctoken { get; set; }
+        private IConfigurationRoot      __config;
+        private DiscordClient           __discord;
+        private CommandsNextModule      __commands;
+        private InteractivityModule     __interactivity;
+
+        static async Task Main(string[] args) => await new Program().InitBot(args);
+        async Task InitBot(string[] args)
+        {
+            Console.WriteLine("[INIT] Cycliq Bot Framework Loading...");
+            __ctoken = new CancellationTokenSource();
+            
+            Console.WriteLine("[INIT] Loading configuration from JSON...");
+            __config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Console.WriteLine("[INIT] Creating Discord Client");
+            __discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = __config.GetValue<string>("discord:token"),
+                TokenType = TokenType.Bot
+            });
+            
+        }
+    }
+}
